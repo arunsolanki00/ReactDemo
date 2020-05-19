56ff58134dcaba38851dda1989ad5e9838c1ae65
Arun Solanki <arun.solanki00@gmail.com>
2020-05-19 16:31:07 +0530

    Google Login Demo

---
+++ logindemo/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+// import './index.css';
+import GoogleLogin from './components/GoogleLogin';
+function App() {
+    return (
+      <div className="App">
+          <GoogleLogin />
+      </div>
+          );
+    }
+
+export default App;
